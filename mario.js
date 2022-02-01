40a2714c584074f34831fb2f6593d8d3388a1835
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2022-01-31 21:07:13 -0500

    Update mario.js

--- mario.js
+++ mario.js
@@ -1,21 +1,22 @@
 function piso(nPisos,altura){
-    //to do
+    let texto = <p></p>
     return"<p>" + "#" + "</p>";
 }
 let nPisos;
+let altura;
 do{
 nPisos=Number(prompt("ingrese el numero de pisos"))
 }while(nPisos%1 !=0||nPisos>10||nPisos<=0||isNaN(nPisos))
+do{
+altura=Number(prompt("ingrese la altura"))
+}while(altura%1 !=0||altura>10||altura<=0||isNaN(altura))
 let contenido="";
-for(let i=1;i<=nPisos;i++){
-    contenido=contenido+piso(i,nPisos)
+
+for(let i=1;i<altura-nPisos;texto+'&nbsp'){
+    return texto
 }
+for(let i = 1;nPisos;texto+"#")
+
 
 let contenedor=document.querySelector("#contenedor");
 contenedor.innerHTML= contenido;
-
-
-
-
-
-
